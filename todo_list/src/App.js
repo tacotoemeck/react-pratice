8d743262cfe0715da41340bbdee5e60da32f46fa
Tomek Wagner <wagner.tomek@gmail.com>
2020-01-22 17:08:15 +0000

    added competed exercise files

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const TodoItem = ({ text }) => (
+  <li>{text}</li>
+)
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: [],
+      newTodo: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const todos = [...this.state.todos, this.state.newTodo];
+    this.setState({ todos, newTodo: '' })
+  }
+
+  render() {
+    const { newTodo } = this.state;
+    const todos = this.state.todos.map((t, i) => (
+      <TodoItem key={i} text={t} />
+    ))
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Task:
+          <input
+              type="text"
+              name="newTodo"
+              onChange={this.handleChange}
+              value={newTodo} />
+          </label>
+          <input type="submit" value="submit" />
+        </form>
+        <div>
+          <ol>
+            {todos}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+
 }
 
-export default App;
+export default App
+
